ede420dfa5cd72dac029df28fbf2128ef827c551
Glib Kozyryatskyy <glib.k@swirldslabs.com>
2025-05-01 21:50:24 +0300

    UI buttons + error handling
    Signed-off-by: Glib Kozyryatskyy <glib.k@swirldslabs.com>

---
+++ src/main/java/com/hasharts/service/fe/CustomErrorHandler.java
@@ -0,0 +1,21 @@
+package com.hasharts.service.fe;
+
+import com.vaadin.flow.component.UI;
+import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.notification.Notification.Position;
+import com.vaadin.flow.server.ErrorEvent;
+import com.vaadin.flow.server.ErrorHandler;
+import lombok.extern.jbosslog.JBossLog;
+
+@JBossLog
+public class CustomErrorHandler implements ErrorHandler {
+
+    @Override
+    public void error(ErrorEvent errorEvent) {
+        Throwable t = errorEvent.getThrowable();
+        log.error(errorEvent.getThrowable());
+        if(UI.getCurrent() != null) {
+            UI.getCurrent().access(() -> Notification.show("Error. Message: " + t.getMessage(), 5000, Position.TOP_CENTER));
+        }
+    }
+}
--- src/main/java/com/hasharts/service/fe/MainView.java
+++ src/main/java/com/hasharts/service/fe/MainView.java
@@ -1,10 +1,11 @@
 package com.hasharts.service.fe;
 
 import com.hasharts.db.model.nft.Image;
+import com.hasharts.db.model.nft.Nft;
 import com.hasharts.db.model.nft.Token;
+import com.hasharts.service.hedera.HederaNftService;
 import com.hasharts.service.image.GoogleImageGenerator;
 import com.vaadin.flow.component.Component;
-import com.vaadin.flow.component.Text;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.grid.Grid;
@@ -20,34 +21,43 @@ import com.vaadin.flow.data.renderer.LitRenderer;
 import com.vaadin.flow.data.renderer.Renderer;
 import com.vaadin.flow.function.SerializableSupplier;
 import com.vaadin.flow.router.Route;
+import com.vaadin.flow.server.VaadinSession;
 import jakarta.inject.Inject;
+import jakarta.transaction.Transactional;
 import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
 import lombok.extern.jbosslog.JBossLog;
 import org.eclipse.microprofile.config.ConfigProvider;
 
+@SuppressWarnings("unused")
 @JBossLog
 @Route("")
 public class MainView extends VerticalLayout {
 
     private final GoogleImageGenerator imageGenerator;
+    private final HederaNftService nftService;
     private final String gatewayPrefix;
     private final String gatewayLocalPrefix;
 
     private final Grid<Token> tokenGrid;
     private Token selectedToken;
+
     private final Grid<Image> imageGrid;
     private final TextField aiTest;
     private Image selectedImage;
 
+    private final Grid<Nft> nftGrid;
+
     @Inject
-    public MainView(GoogleImageGenerator imageGenerator) {
+    public MainView(GoogleImageGenerator imageGenerator, HederaNftService nftService) {
         // config
         this.imageGenerator = imageGenerator;
+        this.nftService = nftService;
         this.gatewayPrefix = ConfigProvider.getConfig().getValue("nft.token.mint.gateway.prefix", String.class);
-        this.gatewayLocalPrefix = ConfigProvider.getConfig().getValue("nft.token.mint.gateway.local.prefix", String.class);
-        log.infof("MainView:%s", imageGenerator);
+        this.gatewayLocalPrefix = ConfigProvider.getConfig()
+                .getValue("nft.token.mint.gateway.local.prefix", String.class);
+        log.infof("MainView: Image:%s Nft:%s", imageGenerator, nftService);
         // Token
         tokenGrid = tokenGrid();
         // Image
@@ -57,8 +67,11 @@ public class MainView extends VerticalLayout {
         aiTest.setPlaceholder("Enter text for AI image generation");
         aiTest.setWidth("800px");
         // generate Image
-        Button save = new Button("Save", e -> {
+        Button generate = new Button("Generate", e -> {
             try {
+                if (aiTest.getValue().trim().isEmpty()) {
+                    throw new RuntimeException("Enter text for AI image generation");
+                }
                 this.imageGenerator.generateAndUpload("nft-image", aiTest.getValue());
                 updateImageGrid();
             } catch (IOException ex) {
@@ -66,16 +79,34 @@ public class MainView extends VerticalLayout {
             }
         });
         // mint nft
-        HorizontalLayout hImages = new HorizontalLayout(aiTest, save);
+        Button mint = new Button("Mint", e -> {
+            if (selectedToken == null) {
+                throw new RuntimeException("Token not selected");
+            }
+            if (selectedImage == null) {
+                throw new RuntimeException("Image not selected");
+            }
+            try {
+                this.nftService.mint(selectedToken, selectedImage);
+            } catch (Exception ex) {
+                throw new RuntimeException(ex);
+            }
+            updateImageGrid();
+        });
+        // Nft
+        nftGrid = nftGrid();
+        HorizontalLayout hImages = new HorizontalLayout(aiTest, generate, mint);
         VerticalLayout vImages = new VerticalLayout(hImages, imageGrid);
         // Tabs
         TabSheet tabSheet = new TabSheet();
         tabSheet.setSizeUndefined();
         tabSheet.setWidth("100%");
+        tabSheet.setHeight("2200px");
         tabSheet.add("Tokens", new LazyComponent(() -> tokenGrid));
         tabSheet.add("Images", new LazyComponent(() -> vImages));
-        tabSheet.add("NFTs", new Div(new Text("This is the NFTs tab content")));
+        tabSheet.add("NFTs", new LazyComponent(() -> nftGrid));
         add(tabSheet);
+        addErrorHandling();
     }
 
     public Grid<Token> tokenGrid() {
@@ -93,7 +124,7 @@ public class MainView extends VerticalLayout {
             selected.ifPresent(token -> this.selectedToken = token);
             System.out.println("Selected token: " + selectedToken.getName());
         });
-
+        grid.setAllRowsVisible(true);
         List<Token> tokens = Token.<Token>findAll().list();
         grid.setItems(tokens);
         return grid;
@@ -112,10 +143,15 @@ public class MainView extends VerticalLayout {
         grid.addColumn(Image::getIpfs).setHeader("Ipfs Hash (CID)")
                 .setAutoWidth(true).setFlexGrow(0);
         grid.addComponentColumn(item -> {
-            Button btn = new Button(new Icon(VaadinIcon.TRASH), click -> Image.deleteById(item.getId()));
+            Button btn = new Button(new Icon(VaadinIcon.TRASH), click -> {
+                log.infof("Delete image: %s", item.getId());
+                deleteImage(item.getId());
+                updateImageGrid();
+            });
             btn.addThemeVariants(ButtonVariant.LUMO_ICON);
             return btn;
         });
+        grid.setAllRowsVisible(true);
         grid.addSelectionListener(selection -> {
             Optional<Image> selected = selection.getFirstSelectedItem();
             selected.ifPresent(image -> this.selectedImage = image);
@@ -129,9 +165,27 @@ public class MainView extends VerticalLayout {
         imageGrid.setItems(images);
     }
 
+    @Transactional
+    public void deleteImage(Long id) {
+        Image.deleteById(id);
+    }
+
+    public Grid<Nft> nftGrid() {
+        Grid<Nft> grid = new Grid<>(Nft.class, false);
+        grid.addColumn(Nft::getHederaTokenId).setHeader("Hedera Token Id")
+                .setAutoWidth(true).setFlexGrow(0);
+        grid.addColumn(Nft::getSerials).setHeader("Serials")
+                .setAutoWidth(true).setFlexGrow(0);
+        grid.addColumn(Nft::getData).setHeader("Data")
+                .setAutoWidth(true).setFlexGrow(0);
+        grid.setAllRowsVisible(true);
+        return grid;
+    }
+
     private static Renderer<Image> createEmployeeRenderer(String prefix) {
         return LitRenderer.<Image>of(
-                        "<vaadin-avatar img=\"${item.url}\" name=\"${item.name}\" alt=\"Image\"></vaadin-avatar>")
+//                        "<vaadin-avatar img=\"${item.url}\" name=\"${item.name}\" alt=\"Image\"></vaadin-avatar>")
+                        "<img src=\"${item.url}\" name=\"${item.name}\" width=\"100\" alt=\"Image\"></img>")
                 .withProperty("url", e -> prefix + e.getIpfs())
                 .withProperty("name", Image::getName);
     }
@@ -144,5 +198,9 @@ public class MainView extends VerticalLayout {
                 }
             });
         }
+    }
+
+    private void addErrorHandling() {
+        VaadinSession.getCurrent().setErrorHandler(new CustomErrorHandler());
     }
 }
